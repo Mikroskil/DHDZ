e4d8bfbb0f9322fea283106cce6408f7f1442495
denly93 <denz_93@yahoo.com>
2013-12-17 17:10:02 +0700

    mobile camera
    fungsi fungsi camera dan upload gambar dengan json

--- sipepeamoeba/Sipepeamoeba/gen/com/example/sipepeamoeba/R.java
+++ sipepeamoeba/Sipepeamoeba/gen/com/example/sipepeamoeba/R.java
@@ -28,26 +28,28 @@ public final class R {
         public static final int orang=0x7f020004;
     }
     public static final class id {
-        public static final int action_settings=0x7f08001a;
+        public static final int TextView01=0x7f08000f;
+        public static final int action_settings=0x7f080021;
         public static final int btnBatal=0x7f08000a;
-        public static final int btnLinkToLoginScreen=0x7f080018;
+        public static final int btnLinkToLoginScreen=0x7f080019;
         public static final int btnLinkToRegisterScreen=0x7f08000c;
-        public static final int btnLogout=0x7f080011;
+        public static final int btnLogout=0x7f080013;
         public static final int btnMasuk=0x7f080009;
-        public static final int btnRegister=0x7f080017;
+        public static final int btnRegister=0x7f080018;
         public static final int btnShowLocation=0x7f08000e;
-        public static final int btnUpload=0x7f080010;
-        public static final int button1=0x7f080019;
-        public static final int cek_error=0x7f080012;
+        public static final int btnUpload=0x7f080012;
+        public static final int btnambilgambar=0x7f08001c;
+        public static final int btnsimpan=0x7f08001f;
         public static final int editId=0x7f080005;
         public static final int editPassword=0x7f080007;
         public static final int editcari=0x7f08000d;
-        public static final int imageView1=0x7f08000f;
+        public static final int edt_meter=0x7f08001e;
+        public static final int imgPreview=0x7f08001d;
         public static final int login_error=0x7f08000b;
-        public static final int registerEmail=0x7f080014;
-        public static final int registerName=0x7f080013;
-        public static final int registerPassword=0x7f080015;
-        public static final int register_error=0x7f080016;
+        public static final int registerEmail=0x7f080015;
+        public static final int registerName=0x7f080014;
+        public static final int registerPassword=0x7f080016;
+        public static final int register_error=0x7f080017;
         public static final int tableRow1=0x7f080004;
         public static final int tableRow2=0x7f080006;
         public static final int tableRow3=0x7f080008;
@@ -55,6 +57,11 @@ public final class R {
         public static final int textView2=0x7f080001;
         public static final int textView3=0x7f080002;
         public static final int textView4=0x7f080003;
+        public static final int tx_cust=0x7f08001b;
+        public static final int tx_encode=0x7f080020;
+        public static final int tx_ket=0x7f08001a;
+        public static final int tx_lokasi_cust=0x7f080011;
+        public static final int tx_lokasi_ptgs=0x7f080010;
     }
     public static final class layout {
         public static final int activity_login=0x7f030000;
--- sipepeamoeba/Sipepeamoeba/src/com/example/sipepeamoeba/Utama.java
+++ sipepeamoeba/Sipepeamoeba/src/com/example/sipepeamoeba/Utama.java
@@ -1,6 +1,11 @@
 package com.example.sipepeamoeba;
 
 
+import java.text.DecimalFormat;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.zip.Checksum;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -10,6 +15,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -21,12 +27,12 @@ import library.UserFunctions;
 
 public class Utama extends Activity {
 	UserFunctions userFunctions;
-	Button btnLogout,btnShowLocation;
+	Button btnLogout,btnShowLocation,btnUpload;
 	EditText id;
 	private final Context CONTEXT = this;
 	public GPSTracker gps;
 	
-	private TextView loginErrorMsg,editcarierror;
+	private TextView loginErrorMsg,tx_lokasi_cust,tx_lokasi_ptgs;
 	// JSON Respon Nama Nodes
 	private static String KEY_SUCCESS = "success";
 	private static String KEY_ERROR = "error";
@@ -34,24 +40,56 @@ public class Utama extends Activity {
 	private static String KEY_ID = "id";
 	private static String KEY_LATITUTE = "latitute";
 	private static String KEY_LONGITUDE = "longitude";
-	private static Double CEK_LATITUTE = 0.0;
-	private static Double CEK_LONGITUDE = 0.0;
-	
+	private static Double CEK_LATITUTE = 0.00;
+	private static Double CEK_LONGITUDE = 0.00;
+	private static String KEY_NAMA = "nama";
+	private static Double latitute = 0.00;
+	private static Double longitude = 0.00;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
-        
-            
         // Cek status login di database
         userFunctions = new UserFunctions();
         if(userFunctions.isUserLoggedIn(getApplicationContext())){
         	setContentView(R.layout.pencarian_lokasi);
         	btnLogout = (Button) findViewById(R.id.btnLogout);
+        	btnShowLocation = (Button)findViewById(R.id.btnShowLocation);
+        	btnUpload = (Button)findViewById(R.id.btnUpload);
+        	id = (EditText)findViewById(R.id.editcari);
+        	
+        	tx_lokasi_cust = (TextView)findViewById(R.id.tx_lokasi_cust);
+        	tx_lokasi_ptgs = (TextView)findViewById(R.id.tx_lokasi_ptgs);
+        	
+        	gps = new GPSTracker(CONTEXT);
+        	
+        	
+        	
+        	//Declare the timer
+        	Timer t = new Timer();
+        	//Set the schedule function and rate
+        	t.scheduleAtFixedRate(new TimerTask() {
+				
+				@Override
+				public void run() {
+					// TODO Auto-generated method stub
+					runOnUiThread(new Runnable() {
+
+		        	    @Override
+		        	    public void run() {
+		        	    	if(gps.canGetLocation()){
+		        	        	check_gps();
+		        	        }else{
+		        	        	gps.showSettingsAlert();
+		        	        }
+		        	    }
+		        	     
+		        	});
+				}
+			}, 0, 1000);
+        	
         	
         	btnLogout.setOnClickListener(new View.OnClickListener() {
-    			
     			public void onClick(View arg0) {
     				// TODO Auto-generated method stub
     				userFunctions.logoutUser(getApplicationContext());
@@ -63,97 +101,68 @@ public class Utama extends Activity {
     			}
     		});
         	
-        	
-        	btnShowLocation = (Button)findViewById(R.id.btnShowLocation);
-        	id = (EditText)findViewById(R.id.editcari);
-        	editcarierror = (TextView)findViewById(R.id.cek_error);
-        	
-        			
             btnShowLocation.setOnClickListener(new View.OnClickListener() {
-            	
     			@Override
     			public void onClick(View v) {
-    				
-    				
     				String kirimid = id.getText().toString();        
-    				
-    			    
     				new MyAsyncTask().execute(kirimid);
     				
-    			    
-    				gps = new GPSTracker(CONTEXT);
-    				
-//    	        	gps = new GPSTracker(this);
-
-    				// check if GPS enabled		
-    		        if(gps.canGetLocation()){
-    		        	
-    		        	double latitude = gps.getLatitude();
-    		        	double longitude = gps.getLongitude();
-    		        	
-    		        	// \n is for new line
-    		        	//editcari.setText("lat:" + latitude);
-    		        	
-    		        	Toast.makeText(getApplicationContext(), "Your Location is - \nLat: " + latitude + "\nLong: " + longitude, Toast.LENGTH_LONG).show();	
-    		        }else{
-    		        	// can't get location
-    		        	// GPS or Network is not enabled
-    		        	// Ask user to enable GPS/network in settings
-    		        	gps.showSettingsAlert();
-    		        }
     			}
     		});
+            
+            btnUpload.setOnClickListener(new View.OnClickListener() {
+				
+				@Override
+				public void onClick(View v) {
+					// TODO Auto-generated method stub
+					if(gps.canGetLocation()){
+						check_gps();
+						//Toast.makeText(getApplicationContext(), latitute.compareTo(CEK_LATITUTE) + " " + longitude.compareTo(CEK_LONGITUDE) + " " + CEK_LATITUTE + " " + CEK_LONGITUDE, Toast.LENGTH_LONG).show();
+			        	
+			        	if(latitute.compareTo(CEK_LATITUTE) == 1 || longitude.compareTo(CEK_LONGITUDE) == 1){
+			        		Toast.makeText(getApplicationContext(), "Lokasi anda dan lokasi customer belum tepat", Toast.LENGTH_LONG).show();
+			        	}
+			        	else{
+			        		Intent upload_foto = new Intent(getApplicationContext(), Upload_foto.class);
+			            	upload_foto.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+			            	upload_foto.putExtra("id_cust",  id.getText().toString());
+			            	startActivity(upload_foto);
+			            	
+			        	}
+			        }else{
+			        	gps.showSettingsAlert();
+			        }
+				}
+			});
+            
+            if(gps.canGetLocation()){
+            	check_gps();
+	        }else{
+	        	gps.showSettingsAlert();
+	        }
+            
             
             
         }else{
         	// apabila status tidak login muncul ke halaman login
-        	
-        	
         	Intent login = new Intent(getApplicationContext(), Login.class);
         	login.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         	startActivity(login);
-        	
-        	
         	// tutup pencarian lokasi
         	finish();
-        	
-        	
         }
     }
     
-	/*private class MyAsyncTask extends AsyncTask<String, Void, JSONObject> {
-	       
-        protected JSONObject doInBackground(String... params) {
-                UserFunctions userFunction = new UserFunctions();
-                if (params.length != 2)
-                        return null;
-                JSONObject json = userFunction.cekLokasi(params[0]);
-                return json;
-        }
-       
-        protected void onPostExecute(JSONObject json) {
-                try {
-            if (json != null && json.getString(KEY_SUCCESS) != null) {
-                loginErrorMsg.setText("");
-                String res = json.getString(KEY_SUCCESS);
-                double lat = Double.parseDouble(json.getString(KEY_LATITUTE));
-                double lon = Double.parseDouble(json.getString(KEY_LONGITUDE));
-                
-                if(Integer.parseInt(res) == 1){
-                    CEK_LATITUTE = lat;
-                    CEK_LONGITUDE = lon;
-                    Toast.makeText(getApplicationContext(), lat + " " +lon , Toast.LENGTH_LONG).show();
-                    
-                }else{
-                	Toast.makeText(getApplicationContext(), "Customer tidak ditemukan" , Toast.LENGTH_LONG).show();
-                }
-            }
-	        } catch (JSONException e) {
-	            e.printStackTrace();
-	        }
-        }
-	}*/
-    private class MyAsyncTask extends AsyncTask<String, Void, JSONObject> {
+	
+    private void check_gps() {
+		// TODO Auto-generated method stub
+    	latitute = (double) (Math.round(gps.getLatitude() * 100.0) / 100.0);
+    	longitude = (double) (Math.round(gps.getLongitude() * 100.0) / 100.0);
+    	tx_lokasi_ptgs.setText("Lokasi Anda  \nlatitute :" + latitute + "\nlongitude :" +longitude  );
+	}
+
+
+	private class MyAsyncTask extends AsyncTask<String, Void, JSONObject> {
 	       
         protected JSONObject doInBackground(String... params) {
                 UserFunctions userFunction = new UserFunctions();
@@ -166,7 +175,9 @@ public class Utama extends Activity {
        
         protected void onPostExecute(JSONObject json) {
 	        try {
-	        	editcarierror.setText(json.getString("latitute"));
+	        	tx_lokasi_cust.setText("Customer tidak ditemukan\n\n");
+	        	CEK_LATITUTE = 0.0;
+                CEK_LONGITUDE = 0.0;
 	        	//editcarierror.setText("a");
 	            /*if (json != null && json.getString(KEY_SUCCESS) != null) {
 	                
@@ -176,6 +187,22 @@ public class Utama extends Activity {
 	                editcarierror.setText(json_user.getString("name"));
 	                
 	            }*/
+	        	if (json != null && json.getString(KEY_SUCCESS) != null) {
+	                
+	                String res = json.getString(KEY_SUCCESS);
+	                double lat = Double.parseDouble(json.getString(KEY_LATITUTE));
+	                double lon = Double.parseDouble(json.getString(KEY_LONGITUDE));
+	                String nama = json.getString(KEY_NAMA);
+	                
+	                if(Integer.parseInt(res) == 1){
+	                	
+	                    CEK_LATITUTE = (double) (Math.round(lat * 100.0) / 100.0);
+	                    CEK_LONGITUDE = (double) (Math.round(lon * 100.0) / 100.0);
+	                    //Toast.makeText(getApplicationContext(), lat + " " +lon , Toast.LENGTH_LONG).show();
+	                    tx_lokasi_cust.setText("Nama Customer : " + nama +"\nLokasi customer  \nlatitute :" + lat + "\nlongitude :" +lon );
+	                	
+	                }
+	            }
 	        } catch (JSONException e) {
 	            e.printStackTrace();
 	        }
